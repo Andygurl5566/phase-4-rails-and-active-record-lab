a53b23efcef8edc803f5ea53178757c385962bb7
Andrea Freeman <afreeman5566@gmail.com>
2021-11-20 16:42:49 -0600

    commit tree 876adb609cabd0cfb568a923c1f7a532d560f7a1

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,4 +4,6 @@
 # Examples:
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+#   Character.create(name: 'Luke', movie: movies.first
+
+Student.create!(first_name: "Jimmy", last_name:"Nuetron", grade: 7)
